52f8acfa9d2c55f85269b9b80286ffb3330f56e8
Vickee Shulman Wojciechowski <vickeeshulmanwojciechowski@Vickees-MacBook-Air.local>
2020-12-13 17:58:25 -0500

    notes to html

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Assignment Code for all arrays - numbers, special character, upper and lower
 var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = [
   '@',
@@ -25,14 +25,14 @@ var specialCharacters = [
   '_',
   '.'
 ];                     //0   // 1     //2    //3
-var lowerCaseArray = ["a", "b" , "c", "d"]
+var lowerCaseArray = ["a", "b", "c", "d"]
 /// add lowercase and uppercase array
 
 
 
 
-function randomize (chosenArray) {
-  var indexNumber = Math.floor(Math.random()* chosenArray.length); //grabs a random index number from chosen array
+function randomize(chosenArray) {
+  var indexNumber = Math.floor(Math.random() * chosenArray.length); //grabs a random index number from chosen array
 
   var indexValue = chosenArray[indexNumber];//gives use a value based on randomIndex number
 
@@ -42,7 +42,7 @@ function randomize (chosenArray) {
 }
 
 
-
+// all user choices are below 
 
 function userChoices() {
   var passwordLength = parseInt(prompt("how long would like your password to be?"));//parseInt changes string to integer
@@ -54,56 +54,59 @@ function userChoices() {
     alert("Must be less than 128")
     return;
   }
-//returns boolean value and stores it in variable
-  var hasNumbers = confirm ("Would you like to include numbers?");
+  //returns boolean value and stores it in variable
+  var hasNumbers = confirm("Would you like to include numbers?");
   var hasSpecialCharacters = confirm("Would you like to include special characters?");
   /// create for upper and lowercase 
 
   var userOptions = { //takes in prompts/confirms and makes them usable values
-    passwordLength:passwordLength,
-    hasNumbers:hasNumbers,
-    hasSpecialCharacters:hasSpecialCharacters,
+    passwordLength: passwordLength,
+    hasNumbers: hasNumbers,
+    hasSpecialCharacters: hasSpecialCharacters,
     // create for upper and lower
 
   }
 
-console.log(userOptions)
-return userOptions;
+  console.log(userOptions)
+  return userOptions;
 
 
 }
 
-function generatePassword () {
+
+// actually generates password w/ input choices 
+
+function generatePassword() {
   var passwordChoices = userChoices();
- var newPassword = [];
- var possibleOptions = [];
+  var newPassword = [];
+  var possibleOptions = [];
 
- if (passwordChoices.hasNumbers) {
-  possibleOptions = possibleOptions.concat(numbersArray);
-  possibleOptions.push(randomize(numbersArray));
- }
- if (passwordChoices.hasSpecialCharacters) {
-  possibleOptions = possibleOptions.concat(specialCharacters);
-  possibleOptions.push(randomize(specialCharacters));
- }
-for(var i = 0; i < passwordChoices.passwordLength; i++) {
-  var openCharacters = randomize(possibleOptions);
-  newPassword.push(openCharacters);
+  if (passwordChoices.hasNumbers) {
+    possibleOptions = possibleOptions.concat(numbersArray);
+    possibleOptions.push(randomize(numbersArray));
+  }
+  if (passwordChoices.hasSpecialCharacters) {
+    possibleOptions = possibleOptions.concat(specialCharacters);
+    possibleOptions.push(randomize(specialCharacters));
+  }
+  for (var i = 0; i < passwordChoices.passwordLength; i++) {
+    var openCharacters = randomize(possibleOptions);
+    newPassword.push(openCharacters);
+  }
+  console.log(newPassword)
+  var assemble = newPassword.join("")
+  return assemble;
 }
-console.log(newPassword)
-var assemble = newPassword.join("")
-return assemble;
-}
 
 
 
-
+// button - should not have to change
 
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(); // change back to generate password 
+  var password = generatePassword(); 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
